80faf2bc132490eb44ea47c014831046ceca46e1
MBehtemam <ehtemam@gmail.com>
2019-02-03 23:42:36 +0330

    some build test

--- src/logic/Store/index.js
+++ src/logic/Store/index.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../Reducers';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(reduxThunk)));
+const store = createStore(rootReducer, applyMiddleware(reduxThunk));
 
 export default store;
