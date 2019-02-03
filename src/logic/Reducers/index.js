983847a65a2193543273a232bda201f9ccd0ca9a
MBehtemam <ehtemam@gmail.com>
2019-02-03 19:34:24 +0330

    combine reducers

---
+++ src/logic/Reducers/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from 'redux';
+import incidents from './incidentsReducer';
+import lastPageFetchNumber from './lastPageFetchNumberReducer';
+import loading from './loadingReducer';
+import occurredAfter from './occurredAfter';
+import occurredBefore from './occurredBefore';
+import perPage from './perPageReducer';
+import proximity from './proximityReducer';
+import query from './queryReducer';
+
+const reducers = combineReducers({
+  incidents,
+  lastPageFetchNumber,
+  loading,
+  occurredAfter,
+  occurredBefore,
+  perPage,
+  proximity,
+  query
+});
+export default reducers;
