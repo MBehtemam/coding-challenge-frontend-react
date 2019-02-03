19993d002ef521a922ecf72a7a188e2a48627640
MBehtemam <ehtemam@gmail.com>
2019-02-03 18:05:14 +0330

    adding occuredAfter actions and test

---
+++ src/logic/Actions/occuredAfterActions.js
@@ -0,0 +1,17 @@
+import {OCCURRED_AFTER_CLEAR,OCCURRED_AFTER_SET} from '../Constants/ActionTypes'
+
+/**
+ * this action clear occurred after
+ */
+export const clearOccurredAfter = () =>({
+    type:OCCURRED_AFTER_CLEAR
+})
+
+/**
+ * this method get time and set is as occurred after
+ * @param {Number} time time that you want to set as  occurred after
+ */
+export const setOccurredAfter = (time)=>({
+    type:OCCURRED_AFTER_SET,
+    payload:time
+})
---
+++ src/logic/Actions/occuredAfterActions.spec.js
@@ -0,0 +1,14 @@
+import { OCCURRED_AFTER_CLEAR, OCCURRED_AFTER_SET } from '../Constants/ActionTypes';
+import { clearOccurredAfter, setOccurredAfter } from './occuredAfterActions';
+
+describe('Test suits for occurredAfter actions', () => {
+  it('should has correct type for clear occurred after', () => {
+    const expectedObj = { type: OCCURRED_AFTER_CLEAR };
+    expect(clearOccurredAfter()).toEqual(expectedObj);
+  });
+  it('should has correct type and payload for set occurred after', () => {
+    const time = Date.now();
+    const expectedObj = { type: OCCURRED_AFTER_SET, payload: time };
+    expect(setOccurredAfter(time)).toEqual(expectedObj);
+  });
+});
