090da6c193f6f827eb66844547549c19b66d6393
MBehtemam <ehtemam@gmail.com>
2019-02-03 14:17:28 +0330

    adding occuredAfter reducer and tests

---
+++ src/logic/Reducers/occurredAfter.js
@@ -0,0 +1,15 @@
+import { OCCURRED_AFTER_CLEAR, OCCURRED_AFTER_SET } from '../Constants/ActionTypes';
+
+export const defaultState = null;
+const occurredAfter = (state = defaultState, action) => {
+  switch (action.type) {
+    case OCCURRED_AFTER_CLEAR:
+      return defaultState;
+    case OCCURRED_AFTER_SET:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default occurredAfter;
---
+++ src/logic/Reducers/occurredAfter.spec.js
@@ -0,0 +1,16 @@
+import { OCCURRED_AFTER_CLEAR, OCCURRED_AFTER_SET } from '../Constants/ActionTypes';
+import occurredAfterReducer, { defaultState } from './occurredAfter';
+
+describe('Test suits for Occurred before reducer', () => {
+  it('should return default state for reducer', () => {
+    expect(occurredAfterReducer(undefined, {})).toBe(defaultState);
+  });
+  it('should clear the state and return default state', () => {
+    expect(occurredAfterReducer(1549176153605, { type: OCCURRED_AFTER_CLEAR })).toBe(null);
+  });
+  it('should set new state', () => {
+    expect(
+      occurredAfterReducer(undefined, { type: OCCURRED_AFTER_SET, payload: 1549176153605 })
+    ).toBe(1549176153605);
+  });
+});
