3c83979d4c9a7214fd0909b849f6f56281a32c12
MBehtemam <ehtemam@gmail.com>
2019-02-05 09:13:40 +0330

    adding custom route

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import IndexPage from '../Layout/IndexPage';
 
 const Routes = () => (
   <Router>
     <Switch>
-      <Route path="/" render={() => <IndexPage />} />
+      <Redirect exact path="/" to="/page/1" />
+      <Route path="/page/:page" render={() => <IndexPage />} />
       <Route path="/case/:id" component={() => <span>Detail</span>} />
     </Switch>
   </Router>
