fc05b82b299b0e2454f04fdc95b4a51017366f2e
MBehtemam <ehtemam@gmail.com>
2019-02-08 18:49:54 +0330

    adding incidents status action types

--- src/components/BikeRow/Thumbnail.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const Thumbnail = styled.img`
-  height: 100px;
-  width: 100px;
-`;
-export default Thumbnail;
--- src/components/BikeRow/Title.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const Title = styled.a`
-  color: blue;
-  font-size: 2rem;
-`;
-
-export default Title;
